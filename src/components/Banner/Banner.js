98f0e298ccf74ce73d8452566b1afc9b0fca1bd3
saim-hossain <saimhossain023@gmail.com>
2022-11-24 00:02:02 +0600

    logo added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import banner from '../../assets/images/banner.jpg'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${banner})` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
